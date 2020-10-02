87ff8fa15f6505ce8815e631b80d18251f5c6ddd
erwingsolorzano <erwing1394@gmail.com>
2020-10-02 00:30:33 -0500

    arreglo de errores menores

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://admin:admin@cluster0.hdngy.gcp.mongodb.net/test',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://admin:admin@cluster0.7j1ro.gcp.mongodb.net/test',
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
