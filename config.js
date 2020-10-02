0cdafc67b4d32c7386be58dd3120165939415043
erwingsolorzano <erwing1394@gmail.com>
2020-10-01 22:50:52 -0500

    commit hecho para cargar en el heroku

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    dbUrl: process.env.DB_URL || 'mongodb+srv://admin:admin@cluster0.hdngy.gcp.mongodb.net/test',
+    port: process.env.PORT || 5000,
+    host: process.env.HOST || 'http://localhost',
+    publicRoute: process.env.PUBLIC_ROUTE || '/',
+    filesRoute: process.env.FILES_ROUTE || 'files',
+}
+
+module.exports = config
